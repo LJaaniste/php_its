ff688eab40988f7294e86309490323df20caa031
LJaaniste <leenajaaniste@gmail.com>
2024-02-12 17:10:23 +0200

    Kodutöö_01.php

---
+++ Kodutöö_nr2.php/Avaleht.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="et">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kodutöö_nr2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        .jumbo{
+            background-image: url(bs3man.jpg);
+            background-size: cover;
+                         
+        }
+        
+       
+  
+    </style> 
+</head>
+  <body>
+    
+       <div class="container">
+        <div class="row">
+        <div class="col-md-6">
+          <i class="far fa-envelope"></i> 
+          <span>sinu@nimi.ee</span>
+          <i class="fas fa-headphones"></i> 
+          <span>+372 555 666</span>
+            </div>
+            
+            <div class="col-md-6 d-flex justify-content-end">
+              <h6>
+                <span class="text-primary">Est</span>
+                <span class="text-dark">|</span>
+                <span class="text-primary">Eng</span>
+                <span class="text-dark">|</span>
+                <span class="text-primary">Rus</span>
+            <a href="#" class="facebook-icon"><i class="fab fa-facebook-f text-dark"></i></a>
+            <a href="#" class="twitter-icon"><i class="fab fa-twitter text-dark"></i></a>
+              </h6>  
+            </div>
+            </div>        
+      
+      <div class="row">
+        <div class="col-md-6">   
+          <div class="logo">
+            <h1><b>Leena Jaaniste</b></h1>
+          </div>  
+        </div>
+        <div class="col-md-6 d-flex justify-content-end">
+             
+          <nav class="navbar navbar-expand-lg">    
+            <a class="navbar-brand" href="#"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Avaleht</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Tooted</a></li>
+            <li class="nav-item"><a class="nav-link"  href="#">Teenused</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Kontakt</a></li>
+            </ul>
+            <button type="button" class="btn btn-primary mx-lg-2 my-2 d-xs-block">Login</button>
+          </div> 
+          </nav>
+          </div>
+              
+        </div>
+      </div>
+                
+        <div class="p-5 bg-body-tertiary jumbo">
+              <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+            <h1 style="color: antiquewhite;"><b>Tõestan, et oskan</b></h1>
+            <h1 style="color: antiquewhite;"><b>Bootstrappi</b></h1>
+            <span class="fs-6" style="color: antiquewhite;">Tegelikult oskan täiega HTML, CSS ja Bootstrap APit. Kahjuks olen</span>
+            <p class="fs-6" style="color: antiquewhite;">liiga laisk!</p>
+            </div>
+            </div>
+          </div>
+          </div>
+       
+        <div class="container">
+          <div class="col-md-12 d-flex align-items-center justify-content-center">
+            <h5 class="mt-4 display-5">Meie teenused</h5>
+            </div>
+          <div class="content py-4 px-4 mx-auto">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="bg-primari text-black text-center">
+                  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+                  <i class="mb-4 fas fa-leaf" style="font-size:48px;color:blue"></i>
+                  <br>
+                  <h5 class="mb-4"><strong>Lorem sipsum</strong></h5>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi velit itaque fuga ducimus, eius aliquam. Dignissimos, ullam. Animi, maiores. A rerum similique iusto distinctio earum numquam corrupti cupiditate aliquam mollitia!</p>
+                  <a href="#" class="primary">Loe edasi</a>
+              </div>
+              </div>
+              <div class="col-md-4">
+                <div class="bg-primari text-black text-center">
+                  <i class="mb-4 fas fa-seedling" style="font-size:48px;color:red"></i>
+                  <br>
+                  <h5 class="mb-4"><strong>Lorem sipsum</strong></h5>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi velit itaque fuga ducimus, eius aliquam. Dignissimos, ullam. Animi, maiores. A rerum similique iusto distinctio earum numquam corrupti cupiditate aliquam mollitia!</p>
+                <a href="#" class="primary">Loe edasi</a>  
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="bg-primari text-black text-center">
+                  <i class="mb-4 fas fa-leaf" style="font-size:48px;color:yellow"></i>
+                  <br>
+                  <h5 class="mb-4"><strong>Lorem sipsum</strong></h5>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi velit itaque fuga ducimus, eius aliquam. Dignissimos, ullam. Animi, maiores. A rerum similique iusto distinctio earum numquam corrupti cupiditate aliquam mollitia!</p>
+                <a href="#" class="primary">Loe edasi</a>   
+                </div>
+              </div>
+            </div>
+          </div>
+        
+          <div class="col-md-12 d-flex align-items-center justify-content-center">
+          <h5 class="display-5">Meie tehtud tööd</h5>
+          </div>
+           
+        <div class="col-md-12 d-flex align-items-center justify-content-center">
+          <p>Seekord pääsesin selliselt</p>
+        </div>
+      
+          <div class="row mt-3 d-flex justify-content-center">
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt1">
+            </div>
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt2">
+            </div>
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt3">
+            </div>
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt4">
+            </div>
+          </div>
+          <div class="row mt-3 d-flex justify-content-center">
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt5">
+            </div>
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt6">
+            </div>
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt7">
+            </div>
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt8">
+            </div>
+          </div>
+          <div class="row mt-3 d-flex justify-content-center">
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt9">
+            </div>
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt10">
+            </div>
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt11">
+            </div>
+            <div class="col-3 d-flex align-items-center justify-content-center">
+              <img src="https://picsum.photos/250/250" class="rounded-circle img-fluid" alt="Pilt12">
+            </div>
+          </div>
+         
+        
+  </div>
+  
+  
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+Wy1q5L4LOhG9cAtI/KD5SBrXbYB/JQOp6u" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </body>
+</html>
---
+++ Kodutöö_nr2.php/Teenused.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="et">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kodutöö_nr2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    </head>
+  <body>
+    
+       <div class="container">
+        <div class="row">
+        <div class="col-md-6">
+          <i class="far fa-envelope"></i> 
+          <span>sinu@nimi.ee</span>
+          <i class="fas fa-headphones"></i> 
+          <span>+372 555 666</span>
+            </div>
+            
+            <div class="col-md-6 d-flex justify-content-end">
+              <h6>
+                <span class="text-primary">Est</span>
+                <span class="text-dark">|</span>
+                <span class="text-primary">Eng</span>
+                <span class="text-dark">|</span>
+                <span class="text-primary">Rus</span>
+            <a href="#" class="facebook-icon"><i class="fab fa-facebook-f text-dark"></i></a>
+            <a href="#" class="twitter-icon"><i class="fab fa-twitter text-dark"></i></a>
+              </h6>  
+            </div>
+            </div>        
+      
+      <div class="row">
+        <div class="col-md-6">   
+          <div class="logo">
+            <h1><b>Leena Jaaniste</b></h1>
+          </div>  
+        </div>
+        <div class="col-md-6 d-flex justify-content-end">
+             
+          <nav class="navbar navbar-expand-lg">    
+            <a class="navbar-brand" href="#"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Avaleht</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Tooted</a></li>
+            <li class="nav-item"><a class="nav-link"  href="#">Teenused</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Kontakt</a></li>
+            </ul>
+            <button type="button" class="btn btn-primary mx-lg-2 my-2 d-xs-block">Login</button>
+          </div> 
+          </nav>
+          </div>
+              
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+Wy1q5L4LOhG9cAtI/KD5SBrXbYB/JQOp6u" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </body>
+</html>
