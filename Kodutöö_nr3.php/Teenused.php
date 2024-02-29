b6b63cf2659c675a58d8d8b2b4136043433d250f
LJaaniste <leenajaaniste@gmail.com>
2024-02-29 11:19:04 +0200

    03.php

--- Kodutöö_nr3.php/Teenused.php
+++ Kodutöö_nr3.php/Teenused.php
@@ -46,7 +46,7 @@
           </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Avaleht</a></li>
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.html">Avaleht</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Tooted</a></li>
             <li class="nav-item"><a class="nav-link active"  aria-current="page" href="teenused.php">Teenused</a></li>
             <li class="nav-item"><a class="nav-link active" aria-current="page" href="kontakt.php">Kontakt</a></li>
