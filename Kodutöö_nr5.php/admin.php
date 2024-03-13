a8566c103001718a4eafe9d2fc4e24c4f28b6848
LJaaniste <leenajaaniste@gmail.com>
2024-03-13 22:31:12 +0200

    05.php

--- Kodutöö_nr5.php/admin.php
+++ Kodutöö_nr5.php/admin.php
@@ -23,7 +23,7 @@
           </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item"><a class="nav-link active" aria-current="page" href="avaleht.php">Avaleht</a></li>
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="avaleht.html">Avaleht</a></li>
             <li class="nav-item"><a class="nav-link active" aria-current="page" href="tehtud.php">Tehtud tööd</a></li>
             <li class="nav-item"><a class="nav-link active" aria-current="page" href="oskused.php">Oskused</a></li>
             <li class="nav-item"><a class="nav-link active" aria-current="page" href="kontakt.php">Kontakt</a></li>
